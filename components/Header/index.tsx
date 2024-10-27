fa1504900cbe884d2544581000de995a0f375cbb
jhnmcndn <johnmcondino@gmail.com>
2024-10-28 04:12:17 +0800

    feat: header development

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -165,7 +165,72 @@ const PopOver = () => {
 };
 
 const Header = () => {
-  return <div>Header Component</div>;
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const [isMoreHover, setIsMoreHover] = useState<boolean>(false);
+
+  const handleMouseEnter = (index: number) => {
+    setHoveredIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredIndex(null);
+  };
+
+  return (
+    <header className={styles.header}>
+      <Link href='/' className={styles.logoWrapper}>
+        <Image src='/logo.png' alt='Logo' width={72} height={72} />
+        <span>Enigma-AI</span>
+      </Link>
+      <nav>
+        <ul>
+          {navList.map((list, index) => (
+            <li
+              key={index}
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={handleMouseLeave}
+              className={clsx({ [styles.isHover]: hoveredIndex === index })}
+            >
+              <Link href={list.navigateTo} className={styles.listWrapper}>
+                <Image
+                  src={list.img}
+                  alt={list.name}
+                  width={32}
+                  height={32}
+                  className={styles.listImg}
+                />
+                <span>{list.name}</span>
+              </Link>
+            </li>
+          ))}
+          <li className={styles.moreWrapper}>
+            <div
+              onMouseEnter={() => setIsMoreHover(true)}
+              onMouseLeave={() => setIsMoreHover(false)}
+              className={clsx(styles.wrapper, {
+                [styles.isMoreHover]: isMoreHover,
+              })}
+            >
+              <Image
+                src='/header/more.png'
+                alt='More'
+                width={32}
+                height={32}
+                className={styles.listImg}
+              />
+              <span className={styles.more}>More</span>
+              <AnimatePresence>{isMoreHover && <PopOver />}</AnimatePresence>
+            </div>
+          </li>
+        </ul>
+      </nav>
+      <span
+        className={`${styles.downloadText} hover:opacity-50 hover:backdrop-blur-[10px] transition-opacity duration-100`}
+      >
+        Download App
+      </span>
+    </header>
+  );
 };
 
 export default Header;
